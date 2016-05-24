5191ea1d769bbd2c043de343a45b041ec2ce3809
J. Tanner Porter <jtporter9@gmail.com>
2016-05-24 01:57:17 -0600

    directive header added

---
+++ js/directives/header/headerCtrl.js
@@ -0,0 +1,11 @@
+angular.module('mySuperClean')
+
+.directive('dirHeader', function () {
+		return {
+			templateUrl: 'js/directives/header/header.html',
+			controller: function($scope){
+
+        
+			}
+		}
+})
