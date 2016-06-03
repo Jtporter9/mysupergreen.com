80926d874311b81eb123cc86db99d95cb3eb5bf4
J. Tanner Porter <jtporter9@gmail.com>
2016-06-02 18:27:05 -0600

    shrinking header almost finished, want to add some animation

--- js/headerShrink.js
+++ js/headerShrink.js
@@ -1,5 +1,9 @@
 (function () {
   $(window).on("scroll touchmove", function () {
-    $('#header_nav').toggleClass('tiny', $(document).scrollTop() > 0);
+    $('#header_nav').toggleClass('tiny', $(document).scrollTop() > 20);
+});
+
+$(window).on("scroll touchmove", function () {
+  $('#main_container_id').toggleClass('new-margin', $(document).scrollTop() > 20);
 });
 })();
