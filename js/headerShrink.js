58c32770c153fad456793fea4b425d755df2341e
J. Tanner Porter <jtporter9@gmail.com>
2016-06-05 21:12:33 -0600

    CNAME mysupergreen.com

--- js/headerShrink.js
+++ js/headerShrink.js
@@ -1,9 +1,10 @@
 (function () {
   $(window).on("scroll touchmove", function () {
-    $('#header_nav').toggleClass('tiny', $(document).scrollTop() > 20);
+    $('#header_nav').toggleClass('tiny', $(document).scrollTop() > 15);
+});
+//For home page
+$(window).on("scroll touchmove", function () {
+  $('#main_container_id').toggleClass('new-margin', $(document).scrollTop() > 15);
 });
 
-$(window).on("scroll touchmove", function () {
-  $('#main_container_id').toggleClass('new-margin', $(document).scrollTop() > 20);
-});
 })();
