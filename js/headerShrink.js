bd74ee4cdb713b74f56bc1ac061e235cf3e6217c
J. Tanner Porter <jtporter9@gmail.com>
2016-06-08 12:09:16 -0600

    styling changes to about page, added content on about page, and changed shrinking header

--- js/headerShrink.js
+++ js/headerShrink.js
@@ -1,10 +1,10 @@
 (function () {
   $(window).on("scroll touchmove", function () {
-    $('#header_nav').toggleClass('tiny', $(document).scrollTop() > 15);
+    $('#header_nav').toggleClass('tiny', $(document).scrollTop() > 10);
 });
 //For home page
 $(window).on("scroll touchmove", function () {
-  $('#main_container_id').toggleClass('new-margin', $(document).scrollTop() > 15);
+  $('#main_container_id').toggleClass('new-margin', $(document).scrollTop() > 10);
 });
 
 })();
